d891a749e092df4cc73195d1b235759e82da961f
pasolre95kim <pasolre95kim@gmail.com>
2018-10-15 18:47:49 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ActiveRecord::Base
+  #is invalid with no name
+  validates :name, presence: true
+  #is invalid with a short number--should be exactly 10 digits
+  validates :phone_number,length: {is: 10}
+  #author names should be unique
+  validates :name, uniqueness: true
+
+
 end
